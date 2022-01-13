0a4f45c5c46722f71a221c11d5c3bbeefc2cb1e3
Aleksandra Marinovic <saska0799@gmail.com>
2022-01-13 20:03:39 +0100

    added form

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,21 @@
+import FeedbackItem from "./FeedbackItem";
+import PropTypes from "prop-types";
+
+function FeedbackList({ feedback, handleDelete }) {
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback Yet</p>;
+  }
+  return (
+    <div className="feedback-list">
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+      ))}
+    </div>
+  );
+}
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.array,
+};
+
+export default FeedbackList;
