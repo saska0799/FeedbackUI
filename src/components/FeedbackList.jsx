5684cb44cedeb19aee4ff0e4e362995f7f51c116
Aleksandra Marinovic <saska0799@gmail.com>
2022-01-15 19:09:25 +0100

    added context

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,21 +1,20 @@
 import FeedbackItem from "./FeedbackItem";
-import PropTypes from "prop-types";
+import { useContext } from "react";
+import FeedbackContext from "../context/FeedbackContext";
 
-function FeedbackList({ feedback, handleDelete }) {
+function FeedbackList() {
+  const { feedback } = useContext(FeedbackContext);
+
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>;
   }
   return (
     <div className="feedback-list">
       {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+        <FeedbackItem key={item.id} item={item} />
       ))}
     </div>
   );
 }
-
-FeedbackList.propTypes = {
-  feedback: PropTypes.array,
-};
 
 export default FeedbackList;
--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,6 +1,7 @@
-import PropTypes from "prop-types";
-
-function FeedbackStats({ feedback }) {
+import { useContext } from "react";
+import FeedbackContext from "../context/FeedbackContext";
+function FeedbackStats() {
+  const { feedback } = useContext(FeedbackContext);
   let average =
     feedback.reduce((acc, cur) => {
       return acc + cur.rating;
@@ -15,9 +16,5 @@ function FeedbackStats({ feedback }) {
     </div>
   );
 }
-
-FeedbackStats.propTypes = {
-  feedback: PropTypes.array.isRequired,
-};
 
 export default FeedbackStats;
